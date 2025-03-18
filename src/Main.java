5abf5d49d334ec076d685f912f65f90d18e3f1e9
EmirhanAyaz <142207806+EmirhanAyaz@users.noreply.github.com>
2025-03-18 14:13:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
- HEAD
+
 
 import java.util.Scanner;
 
- b57839e206fd4e933a6f02b2bfb66b16ebf96a21
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
- HEAD
+
         String name;
         System.out.println("Welcome to BMK JAVA LESSON");
         Scanner veriAl = new Scanner(System.in);
@@ -20,6 +20,6 @@ public class Main {
 
 System.out.println("SA");
 System.out.println("Galiba oldu gibi");
- b57839e206fd4e933a6f02b2bfb66b16ebf96a21
+
         }
     }
