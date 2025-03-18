b390ad5c2ccaac317d3b65ca88b19732dafa1898
EmirhanAyaz <142207806+EmirhanAyaz@users.noreply.github.com>
2025-03-18 13:31:41 +0300

    Güncellemeler yapıldı

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SA");
-        System.out.println("SA");
+        String name;
+        System.out.println("Welcome to BMK JAVA LESSON");
+        Scanner veriAl = new Scanner(System.in);
+        System.out.println("Please enter your name :");
+        name = veriAl.next();
+        System.out.println("Pleased to meet you "+name);
+
 
         }
     }
